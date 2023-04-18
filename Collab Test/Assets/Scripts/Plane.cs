7c876bc6ea85c0be7d7b61a7eca95f72f0ad5ed8
DeathAdder-AU <katabwa07@gmail.com>
2023-04-18 14:53:05 +0800

    game ok

---
+++ Collab Test/Assets/Scripts/Plane.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidBody;
+    public float jumpstrength;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidBody.velocity = Vector2.up * jumpstrength;
+        }
+
+    }
+}
